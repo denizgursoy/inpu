c07de7ea329e8290da371ea8dca9990f36c8de72
denizgursoy <denizgursoy.443@gmail.com>
2025-10-16 21:38:44 +0200

    test closed body

--- response_handlers_test.go
+++ response_handlers_test.go
@@ -3,8 +3,10 @@ package inpu
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
+	"net/http/httptest"
 
 	"github.com/h2non/gock"
 )
@@ -24,98 +26,95 @@ func (c *ClientSuite) Test_Response_UnmarshalJson() {
 	c.Require().Equal(expectedResponse, result)
 }
 
-func (c *ClientSuite) Test_Response_UnmarshalJson_Body_Close() {
-	// var closeCalled atomic.Bool
-	// result := testModel{}
-	// server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
-	// 	w.WriteHeader(http.StatusOK)
-	// 	w.Write([]byte(testDataAsJson))
-	// }))
-	// defer server.Close()
-	//
-	// req := New().Post(server.URL, nil).
-	// 	OnReply(StatusIs(http.StatusOK), UnmarshalJson(&result))
-	//
-	// err := req.Send()
-	// c.Require().NoError(err)
-	// expectedResponse := testModel{
-	// 	Foo: "bar",
-	// }
-	// c.Require().Equal(expectedResponse, result)
-	// c.Require().True(closeCalled.Load())
-}
-
 func (c *ClientSuite) Test_Response_No_Nil_Parameter() {
-	gock.New(testUrl).Post("/").Reply(http.StatusOK).Body(bytes.NewReader([]byte(`{"foo":"bar"}`)))
-	err := Post(testUrl, nil).
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"foo":"bar"}`))
+	}))
+	defer server.Close()
+
+	err := Post(server.URL, nil).
 		OnReply(StatusIs(http.StatusOK), UnmarshalJson(nil)).
 		Send()
+
 	c.Require().ErrorIs(err, ErrMarshalToNil)
 }
 
 func (c *ClientSuite) Test_Response_Parameter_Must_Be_Pointer() {
-	gock.New(testUrl).Post("/").Reply(http.StatusOK).Body(bytes.NewReader([]byte(`{"foo":"bar"}`)))
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"foo":"bar"}`))
+	}))
+	defer server.Close()
+
 	result := testModel{}
-	err := Post(testUrl, nil).
+	err := Post(server.URL, nil).
 		OnReply(StatusIs(http.StatusOK), UnmarshalJson(result)).
 		Send()
 	c.Require().ErrorIs(err, ErrNotPointerParameter)
 }
 
 func (c *ClientSuite) Test_Response_ReturnDefaultError() {
-	gock.
-		New(testUrl).
-		Post("/").
-		Reply(http.StatusInternalServerError).
-		Body(bytes.NewReader([]byte(`{"foo":"bar"}`)))
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"foo":"bar"}`))
+	}))
+	defer server.Close()
 
-	err := Post(testUrl, nil).
+	err := Post(server.URL, nil).
 		OnReply(StatusAny, ReturnDefaultError).
 		Send()
+
 	c.Require().Error(err)
-	c.Require().Equal("called [POST] -> https://my.example.com and got 500", err.Error())
+	c.Require().Equal(fmt.Sprintf("called [POST] -> %s and got 500", server.URL), err.Error())
 }
 
 func (c *ClientSuite) Test_Response_ReturnError() {
-	gock.
-		New(testUrl).
-		Post("/").
-		Reply(http.StatusInternalServerError).
-		Body(bytes.NewReader([]byte(`{"foo":"bar"}`)))
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"foo":"bar"}`))
+	}))
+	defer server.Close()
+
 	expectedError := errors.New("something happened")
-	actualError := Post(testUrl, nil).
+	actualError := Post(server.URL, nil).
 		OnReply(StatusAny, ReturnError(expectedError)).
 		Send()
 	c.Require().ErrorIs(actualError, expectedError)
 }
 
-type spyTransport struct {
-	base        http.RoundTripper
-	onBodyClose func()
-}
+func (c *ClientSuite) Test_Response_Body_Closed() {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"foo":"bar"}`))
+	}))
+	defer server.Close()
 
-func (s *spyTransport) RoundTrip(req *http.Request) (*http.Response, error) {
-	resp, err := s.base.RoundTrip(req)
-	if err != nil {
-		return nil, err
-	}
+	var closedBody *spyBody
+	client := New().UseMiddlewares(ResponseModifierMiddleware(func(response *http.Response, server error) (*http.Response, error) {
+		// Wrap the body
+		closedBody = &spyBody{
+			ReadCloser: response.Body,
+		}
 
-	// Wrap the body
-	resp.Body = &spyBody{
-		ReadCloser: resp.Body,
-		onClose:    s.onBodyClose,
-	}
-	return resp, nil
+		response.Body = closedBody
+		return response, nil
+	}, "test", 12))
+
+	err := client.Post(server.URL, nil).
+		OnReply(StatusAny, DoNothing).
+		Send()
+
+	c.Require().NoError(err)
+	c.Require().True(closedBody.isClosed)
 }
 
 type spyBody struct {
 	io.ReadCloser
-	onClose func()
+	isClosed bool
 }
 
 func (s *spyBody) Close() error {
-	if s.onClose != nil {
-		s.onClose()
-	}
+	s.isClosed = true
 	return s.ReadCloser.Close()
 }
